a11b06c345d704d9e75db1354916b4859fb581dd
Aram Bekverdyan <arambekverdyan@gmail.com>
2017-10-22 22:31:20 +0400

    1. In the main class table creation and actions import were changed into singletone pattern. 2. In the ImportToDB class added singleton pattern for server connection. 3. Code formatting.

--- src/main/java/toDB/ImportMain.java
+++ src/main/java/toDB/ImportMain.java
@@ -1,25 +1,30 @@
 package toDB;
+
 import xlsParser.XlsReader;
 
 import java.io.IOException;
+
 public class ImportMain {
-    private static final String XLSPath = "E:/Paruyr/codes/warehouse/src/main/resources/MTOperationsJournal.xls";
-    private static final String tableName = "action";
-    public static void main(String args[]) throws ClassNotFoundException {
-        XlsReader xlsReader;
-        try {
-            xlsReader = new XlsReader(XLSPath);
-            ImportToDB db = new ImportToDB();
-            db.createTable(tableName);
-            //            xlsReader.printActList();
-              db.importActions(xlsReader.getActionsList());
+  private static final String XLSPath = "E:/Paruyr/codes/warehouse/src/main/resources/MTOperationsJournal.xls";
+  private static final String tableName = "action";
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+  public static void main(String args[]) throws ClassNotFoundException {
+    XlsReader xlsReader;
+    try {
+      xlsReader = new XlsReader(XLSPath);
+      ImportToDB.getInstance().createTable(tableName);
+      ImportToDB.getInstance().importActions(xlsReader.getActionsList());
+      //ImportToDB.getInstance().importActions(gk);
+//      db.createTable(tableName);
+//                  xlsReader.printActList();
+      //db.importActions(xlsReader.getActionsList());
 
-//            xlsReader.printActList();db.importActions(xlsReader.getActionsList());
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+
+    //            xlsReader.printActList();db.importActions(xlsReader.getActionsList());
+  }
 
 
 }
--- src/main/java/xlsParser/Action.java
+++ src/main/java/xlsParser/Action.java
@@ -5,87 +5,96 @@ import java.util.Date;
 
 public class Action {
 
-    private Date date;         // 0 column
-    private String in_out;	     // 1
-    private String partNumber;  //  2
-    private String unit;        //  3
-    private double quantity;    //  4
-    private String partName;    //  5
+  private Date date;         // 0 column
+  private String in_out;       // 1
+  private String partNumber;  //  2
+  private String unit;        //  3
+  private double quantity;    //  4
+  private String partName;    //  5
 
-    ///////////////Empty Constructor/////////////
-    public Action(){
+  ///////////////Empty Constructor/////////////
+  public Action() {
 
-    }
-    //////////////Clone Constructor//////////////
-    public Action(Action action){
-        this.date=action.date;
-        this.in_out=action.in_out;
-        this.partNumber= action.partNumber;
-        this.unit= action.unit;
-        this.quantity= action.quantity;
-        this.partName=action.partName;
-    }
+  }
 
-    ///////////SETTERS///////////////////////////
-    public void setDate( Date date2){
-        date=date2;
-    }
-    public void setUnit(String un){
-        unit= un;
-    }
-    public void setIn_out(String in){
-        in_out=in;
-    }
-    public void setPartNumber(String pNumber){
-        partNumber=pNumber;
-    }
-    public void setPartName(String  pName){
-        partName=pName;
-    }
-    public void setQuantity(double q){
-        quantity = q;
-    }
+  //////////////Clone Constructor//////////////
+  public Action(Action action) {
+    this.date = action.date;
+    this.in_out = action.in_out;
+    this.partNumber = action.partNumber;
+    this.unit = action.unit;
+    this.quantity = action.quantity;
+    this.partName = action.partName;
+  }
 
-    ///////////GETTER////////////////////////////
-    public Date getDate(){
-        return date;
-    }
-        public double getQuantity(){
-        return quantity;
-    }
-    public String getPartName(){
-        return partName;
-    }
-    public String getUnit(){
-        return unit;
-    }
-    public String getIn_out(){
-        return in_out;
-    }
-    public String getPartNumber(){
-        return 	partNumber;
-    }
-    public String getDateInSQLFormat(){
-        SimpleDateFormat sdf= new SimpleDateFormat("yyyy-MM-dd");
-        return sdf.format(getDate());
-    }
-///////////////////////////////////////////////
-    public String toString(){
+  ///////////SETTERS///////////////////////////
+  public void setDate(Date date2) {
+    date = date2;
+  }
 
-        return (getDateInSQLFormat()+" | "+getIn_out()+" | "+getPartNumber()+" | "+getQuantity()+" | "+ getPartName());
-    }
-    ///////////////////////////////////////////////
-    /////////////getInsertQuery/////////////////////////
-    public String createInsertQuery(String tableFullName){
-        Double quantity = getQuantity();
-        return "INSERT INTO " + tableFullName +
-                "(date, in_out, partNumber, unit, quantity, partName) "+
-                "VALUES ( '" +
-                getDateInSQLFormat()+"','"+
-        getIn_out()+"','"+
-                getPartNumber()+"','"+
-                getUnit() +"','"+
-                quantity.toString()+"','"+
-                getPartName() +"')";
-    }
+  public void setUnit(String un) {
+    unit = un;
+  }
+
+  public void setIn_out(String in) {
+    in_out = in;
+  }
+
+  public void setPartNumber(String pNumber) {
+    partNumber = pNumber;
+  }
+
+  public void setPartName(String pName) {
+    partName = pName;
+  }
+
+  public void setQuantity(double q) {
+    quantity = q;
+  }
+
+  ///////////GETTER////////////////////////////
+  public Date getDate() {
+    return date;
+  }
+
+  public double getQuantity() {
+    return quantity;
+  }
+
+  public String getPartName() {
+    return partName;
+  }
+
+  public String getUnit() {
+    return unit;
+  }
+
+  public String getIn_out() {
+    return in_out;
+  }
+
+  public String getPartNumber() {
+    return partNumber;
+  }
+
+  public String getDateInSQLFormat() {
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+    return sdf.format(getDate());
+  }
+
+  ///////////////////////////////////////////////
+  public String toString() {
+
+    return (getDateInSQLFormat() + " | " + getIn_out() + " | " + getPartNumber() + " | " + getQuantity() + " | " +
+        getPartName());
+  }
+
+  ///////////////////////////////////////////////
+  /////////////getInsertQuery/////////////////////////
+  public String createInsertQuery(String tableFullName) {
+    Double quantity = getQuantity();
+    return "INSERT INTO " + tableFullName + "(date, in_out, partNumber, unit, quantity, partName) " + "VALUES ( '" +
+        getDateInSQLFormat() + "','" + getIn_out() + "','" + getPartNumber() + "','" + getUnit() + "','" + quantity
+        .toString() + "','" + getPartName() + "')";
+  }
 }
